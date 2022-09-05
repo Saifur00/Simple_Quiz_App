b14b064b243f7ca00339a1ea61e1a6ed17051864
Saifur00 <saifurbgc@gmail.com>
2022-09-05 17:11:35 +0600

    second commit

--- Quiz2.js
+++ Quiz2.js
@@ -1,4 +1,4 @@
-const correctAnswers = ['Elon Musk', 'Batman', 'Avengers']
+const correctAnswers = ['Elon Musk', 'Batman', 'Avengers', 'Daredevil']
 
 const form = document.getElementById('quizes')
 // const btn = document.getElementById('btnSubmit')
@@ -8,7 +8,7 @@ quizes.addEventListener('submit', e=>{
     e.preventDefault()
     let score = 0;
     let percentage = 0;
-    const userAnswers = [form.richest_man.value, form.super_hero.value, form.comic_movie.value]
+    const userAnswers = [form.richest_man.value, form.super_hero.value, form.comic_movie.value, form.comic_show.value]
 
     userAnswers.forEach((ans, index)=>{
         if(ans === correctAnswers[index])
@@ -19,6 +19,17 @@ quizes.addEventListener('submit', e=>{
             score;
         }
     })
-    percentage = (score/3)*100;
-    output.innerText = `You've Got ${percentage}% On The QUIZ`
+    percentage = (score/4)*100;
+    // output.innerText = `You've Got ${percentage.toFixed(2)}% On The QUIZ`
+    scrollTo({top:0, behavior: "smooth"})
+    let showResult = 0;
+    const timer = setInterval(()=>{
+        output.innerText = `You've Got ${showResult}% On The QUIZ`
+        if(showResult === Math.trunc(percentage)){
+            clearInterval(timer)
+        }
+        else{
+            showResult++;
+        }
+    },20)
 })
